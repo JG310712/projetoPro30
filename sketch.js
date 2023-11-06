4152b9abfbbf3841b43f1bd9d90d85820d743617
joãoguilherme <112426149+JG310712@users.noreply.github.com>
2023-11-06 16:43:00 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,12 +13,12 @@ var ground, bridge;
 var leftWall, rightWall;
 var jointPoint;
 var jointLink;
-var zombie1, zombie2, zombie3, zombie4, sadzombie;
+var zombie;
+var zombie1, zombie2, zombie3, zombie4;
 var breakButton;
 var backgroundImage;
 
 var stones = [];
-var collided = false;
 
 function preload() {
   zombie1 = loadImage("./assets/zombie1.png");
@@ -26,7 +26,6 @@ function preload() {
 
   zombie3 = loadImage("./assets/zombie3.png");
   zombie4 = loadImage("./assets/zombie4.png");
-  sadzombie = loadImage("./assets/sad_zombie.png");
 
   backgroundImage = loadImage("./assets/background.png");
 }
@@ -54,21 +53,16 @@ function setup() {
     stones.push(stone);
   }
 
-  zombie = createSprite(width / 2, height - 100, 50, 50);
+  zombie = createSprite(width / 2, height - 110);
   zombie.addAnimation("lefttoright", zombie1, zombie2, zombie1);
   zombie.addAnimation("righttoleft", zombie3, zombie4, zombie3);
-  zombie.addImage("sad", sadzombie);
-
   zombie.scale = 0.1;
   zombie.velocityX = 10;
 
-  breakButton = createButton("BOTÃO");
+  breakButton = createButton("");
   breakButton.position(width - 200, height / 2 - 50);
   breakButton.class("breakbutton");
-  //breakButton.mouseClicked(handleButtonPress);
   breakButton.mousePressed(handleButtonPress);
-  //breakButton.mouse(handleButtonPress);
-  //breakButton.mousePressed(ButtonPress);
 }
 
 function draw() {
@@ -80,24 +74,21 @@ function draw() {
   for (var stone of stones) {
     stone.show();
   }
+
+  if (zombie.position.x >= width - 300) {
+    zombie.velocityX = -10;
+    zombie.changeAnimation("righttoleft");
+  }
+
+  if (zombie.position.x <= 300) {
+    zombie.velocityX = 10;
+    zombie.changeAnimation("lefttoright");
+  }
+
+  drawSprites();
 }
 
 function handleButtonPress() {
-  /*jointLink=dettach();
-  setTimeout(() => {
-    bridge.break();
-  }, 1500);*/
-
-  /*jointLink.dettach();
-  setTimeout(() => {
-    break();
-  }, 1500);*/
-
-  /*jointLink.dettach();
-  setTimeout(() => {
-    bridge.break();
-  }, 5);*/
-
   jointLink.dettach();
   setTimeout(() => {
     bridge.break();
